cf094240e11ca8fdb244efc1f8c343240f72563f
cactusflashes <121457318+cactusflashes@users.noreply.github.com>
2023-03-10 14:19:12 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+
+console.log("hello, world!");
+
+//API key & database ref
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAqXbrfut6o2Fqf5oPYczouIXEmJnx2f_M",
+    authDomain: "where-is-joseph.firebaseapp.com",
+    databaseURL: "https://where-is-joseph-default-rtdb.firebaseio.com",
+    projectId: "where-is-joseph",
+    storageBucket: "where-is-joseph.appspot.com",
+    messagingSenderId: "114864610218",
+    appId: "1:114864610218:web:bd0419a350fda8409069d3",
+    measurementId: "G-J6ZW0G0ENH"
+  };
+  
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const dropdown = document.getElementById('dropdown-status');
+
+dropdown.addEventListener('change', () => {
+  console.log('Dropdown Value Changed.');
+
+    const selectedOption = dropdown.value.replace("-", '');
+  console.log('Selected option:', selectedOption);
+
+    //store option in database
+    set(ref(database,'selectedOption'), selectedOption)
+    .then(() => {
+      console.log('Option successfully saved to database.');
+    })
+    .catch((error) => {
+      console.log('Error saving option to database:', error);
+    });
+
+});
+
